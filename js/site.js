0368267200515cf14565005878f9e46aeaed4b64
HarrisonW <harrison.c.wong@gmail.com>
2011-01-08 12:55:37 -0500

    Move facebook to about page. Edited content

--- js/site.js
+++ js/site.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
 	$("#years").accordion({autoHeight: false});
+	$("#menu a").each(function() {
+		if($(this).attr("href") == location.href.toLowerCase()) {
+			$(this).addClass("active");
+		}
+	|
 });
